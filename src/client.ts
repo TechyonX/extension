7d04f2d551ed582e4a130907ae2f6e0fd832ad1a
khasbilegt <khasbilegt.ts@gmail.com>
2023-04-15 01:11:06 +0800

    add db types

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,8 @@
 import { LocalStorage } from "@raycast/api";
 import { createClient, SupportedStorage } from "@supabase/supabase-js";
 
+import { Database } from "./types";
+
 const storage: SupportedStorage = {
   getItem: LocalStorage.getItem as (value: string) => Promise<string | null>,
   setItem: LocalStorage.setItem,
@@ -11,4 +13,4 @@ export const supabaseUrl = "https://hpiywkenufslzbcpcijs.supabase.co";
 export const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwaXl3a2VudWZzbHpiY3BjaWpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEwMjMzMjksImV4cCI6MTk5NjU5OTMyOX0.TZqsgeV5HdmGpF2sf6B0meliiJN99I5TNiFO-P3hw1g";
 
-export const supabase = createClient(supabaseUrl, supabaseKey, { auth: { storage } });
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey, { auth: { storage } });
